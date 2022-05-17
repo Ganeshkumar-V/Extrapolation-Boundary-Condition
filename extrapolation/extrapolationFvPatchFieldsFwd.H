10af6f15c6eb33edac2ef39af9000ab68a2b7b05
Ganeshkumar V <90136974+Ganeshkumar-V@users.noreply.github.com>
2022-05-17 15:20:31 +0530

    Update extrapolationFvPatchFieldsFwd.H

--- extrapolation/extrapolationFvPatchFieldsFwd.H
+++ extrapolation/extrapolationFvPatchFieldsFwd.H
@@ -5,7 +5,7 @@
     \\  /    A nd           | www.openfoam.com
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
-    Copyright (C) 2011 OpenFOAM Foundation
+ Copyright (C) 2022 Ganeshkumar-V, PhD Mechanical Engineering, IIT Gandhinagar
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
